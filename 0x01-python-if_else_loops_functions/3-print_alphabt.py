4514e6e6e442240f1cf9bf055cf261a19ada2ce9
MayWilliy <willliamsmadebusola@gmail.com>
2023-08-08 20:20:30 -0700

    python loop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Lowercase letters (ASCII 97 to 122)
+#Lowercase letters (ASCII 97 to 123)
 for ascii_value in range (97, 123):
     if ascii_value != 113 and  ascii_value != 101:
-    print(chr(ascii_value), end=" ")
+    print("{}".format(chr(ascii_value)), end="")
