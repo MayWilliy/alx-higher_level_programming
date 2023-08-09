378f5bc56c3c1b27973206fcf679d2180560b625
MayWilliy <willliamsmadebusola@gmail.com>
2023-08-08 20:38:35 -0700

    python loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range (10):
-    for j in range (i + 1, 10):
-        if (i < 9) and (j < 9):
-        print("{:02d}, ".format(i * 10 + j), end=" ")
+for digit in range (0, 10):
+    for digital in range (digit + 1, 10):
+        if digit == 8 and digital == 9:
+        print("{}{}".format(digit, digital))
     else:
-        print("{:02d}". format(i * 10 + j))
+        print("{}{}".format(digit, digital) end=", ")
