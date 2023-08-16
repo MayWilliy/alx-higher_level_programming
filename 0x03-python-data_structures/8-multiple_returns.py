a12b1580fb6e3d476e575b17ea1c0b179cfa0302
MayWilliy <willliamsmadebusola@gmail.com>
2023-08-15 17:54:57 -0700

    python data struct

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    """This function returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
