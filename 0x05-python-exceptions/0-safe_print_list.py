a675dbab5b7110313f13559521a82e7c723eac02
MayWilliy <willliamsmadebusola@gmail.com>
2023-08-28 17:05:29 -0700

    except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    #function that prints x elements of a list
+    # function that prints x elements of a list
     value = 0
 
     for i in range(x):
