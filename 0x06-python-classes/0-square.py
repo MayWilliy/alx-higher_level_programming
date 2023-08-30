7b118644c6bbd60ea3df6c3b22670c5f66f1df54
MayWilliy <willliamsmadebusola@gmail.com>
2023-08-29 20:20:57 -0700

    class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,4 +5,8 @@ function that create an empty class Square that defines a square
 """
 
 class Square:
-    pass 
+    """
+    This is an empty Square class that defines a square.
+    """
+    pass # The pass statement indicates an empty class with no attributes or methods
+ 
