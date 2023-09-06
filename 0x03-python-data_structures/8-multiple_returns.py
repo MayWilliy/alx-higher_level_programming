7296bd993b771c366a4499550e629a8f26d42659
Mariam Williams <125450777+MayWilliy@users.noreply.github.com>
2023-09-06 01:28:33 +0100

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-def multiple_returns(sentence):
-    """This function returns the length of a string and its first character."""
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
