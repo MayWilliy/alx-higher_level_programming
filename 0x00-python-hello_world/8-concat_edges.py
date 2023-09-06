52e4f280eefeba622517219d21ac826efd5187c5
Mariam Williams <125450777+MayWilliy@users.noreply.github.com>
2023-09-06 01:27:35 +0100

    Delete 0x00-python-hello_world directory

--- 0x00-python-hello_world/8-concat_edges.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[33:67] + str[95:-18] + str[:6]
-print(str)
