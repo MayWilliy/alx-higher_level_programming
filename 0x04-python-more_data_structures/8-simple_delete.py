65948ba07e352f3bb0648d9af857bd7b9b7bf7d2
Mariam Williams <125450777+MayWilliy@users.noreply.github.com>
2023-09-06 01:29:30 +0100

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/8-simple_delete.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def simple_delete(a_dictionary, key=""):
-    '''This function deletes a key in a dictionary'''
-    if a_dictionary.get(key) is not None:
-        del a_dictionary[key]
-    return (a_dictionary)
