d7cc2a051db19c54f81169b6ad3ef2e54fd9c880
root <MayWilliy>
2023-10-03 22:05:26 -0700

    loops

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/pyhton3
 def add(a, b):
     sum = a + b
-    return(sum)
+    return sum
--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
     quotient = a ** b
-    return(quotient)
+    return quotient
