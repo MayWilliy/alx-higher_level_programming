60ebc13aff4ca738baf03835a2dd963da364245c
MayWilliy <williamsmadebusola@gmail.com>
2023-10-09 18:46:26 +0000

    tulpe

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,5 +5,4 @@ def print_list_integer(my_list=[]):
     """
     my_list = [1, 2, 3, 4, 5]
     for item in my_list:
-        print("{}".format(item), end=", ")
-    print()
+        print("{}".format(item), end="\n")
