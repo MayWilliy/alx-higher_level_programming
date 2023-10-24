357f0d100ed9b9c6aa478e3556cffad31ad980fa
MayWilliy <williamsmadebusola@gmail.com>
2023-10-24 05:56:21 +0000

    exceptions

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python function def magic_calculation(a, b)
+"""
+def magic_calculation(a, b):
+	result = 0
+	for i in range(1, 3):
+		try:
+			if i > a:
+				raise Exception('Its too high')
+			else:
+				result += a ** b / i
+		except:
+			result = b + a
+			break
+	return (result)
